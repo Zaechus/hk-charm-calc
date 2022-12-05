6a4ab4555ba05ace5cb3b64aa6f2bcd3494b1cd1
Zaechus <zaechus@proton.me>
2022-12-05 15:37:16 -0700

    update to axum 0.6 and fix

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use tower_http::services::ServeDir;
 #[tokio::main]
 async fn main() {
     let serve_dir = get_service(ServeDir::new("docs")).handle_error(handle_error);
-    let app = Router::new().fallback(serve_dir);
+    let app = Router::new()
+        .fallback_service(serve_dir)
+        .into_make_service();
 
     axum::Server::bind(&"0.0.0.0:7878".parse().unwrap())
-        .serve(app.into_make_service())
+        .serve(app)
         .await
         .unwrap();
 }
