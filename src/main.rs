456b588f1d317d0bb9dc33c863ad2ce4e3a6e879
Zaechus <zaechus@proton.me>
2023-03-05 14:23:54 -0700

    remove nix and update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::get_service, Router};
+use axum::{routing::get_service, Router};
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let serve_dir = get_service(ServeDir::new("docs")).handle_error(handle_error);
+    let serve_dir = get_service(ServeDir::new("docs"));
     let app = Router::new()
         .fallback_service(serve_dir)
         .into_make_service();
@@ -12,8 +12,4 @@ async fn main() {
         .serve(app)
         .await
         .unwrap();
-}
-
-async fn handle_error(_err: std::io::Error) -> impl IntoResponse {
-    StatusCode::INTERNAL_SERVER_ERROR
 }
